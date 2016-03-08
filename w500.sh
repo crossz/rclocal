daf531a55b9c44d4c523f976fd8dc17284139e50
Cross 1.21 <you@example.com>
2016-03-08 19:23:53 +0800

    update: spider services

--- w500.sh
+++ w500.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /opt/spider/500
-phantomjs /opt/spider/500/500.js 0 utf-8 >> /opt/logs/w500_upstart.log 2>&1
+exec phantomjs /opt/spider/500/500.js 0 utf-8 >> /opt/logs/w500_upstart.log 2>&1
 
