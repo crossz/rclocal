c2702d18893cb7ef0cd1887c08d612feeab9d1ad
Cross 1.21 <you@example.com>
2016-03-15 11:06:04 +0800

    correct: spider-web.service.sh

--- spider-web.service.sh
+++ spider-web.service.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 . /etc/profile.d/caiex.sh
 . /opt/spider/profile.d/caiex_matlab.sh
-exec java -jar /opt/spider/matlab-service.jar > /opt/logs/matlab_service.out 2>&1 
-
+exec java -jar /opt/spider/spider-web.war >> /opt/logs/spider-web.out 2>&1
