16fa053d97ff98c0e3a8419dc22cb834667aa58e
Cross 1.21 <you@example.com>
2016-03-16 13:33:22 +0800

    add: spider-web by spring boot for 1.6

---
+++ spider-web.service.16.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+. /etc/profile.d/caiex.sh
+. /opt/spider/profile.d/caiex_matlab.sh
+#exec java -jar /opt/spider/spider-web.war >> /opt/logs/spider-web.out 2>&1
+exec java -jar /opt/spider/spider-web.war --Drocket.mq.addr=192.168.1.154:9876 --Dendpoint.sbc.match_compare=http://192.168.1.6:8080/SBC/matchCompare.do --Dserver.port=8081 >> /opt/logs/spider-web.out 2>&1
--- spider-web.service.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-. /etc/profile.d/caiex.sh
-. /opt/spider/profile.d/caiex_matlab.sh
-exec java -jar /opt/spider/spider-web.war >> /opt/logs/spider-web.out 2>&1
