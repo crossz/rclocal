cac909c0168dff0e14dd18c0cb53fb453bdc9bf3
Cross Z <you@example.com>
2016-03-22 11:03:43 +0800

    modify: test if matlab.sh exist or not

--- tomcat_upstart.sh
+++ tomcat_upstart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . /etc/profile.d/caiex.sh
-. /opt/sbcconsumer/profile.d/caiex_matlab.sh
+[ -e /opt/sbcconsumer ] && . /opt/sbcconsumer/profile.d/caiex_matlab.sh
 exec $CATALINA_HOME/bin/catalina.sh run >> /opt/logs/catalina.out 2>&1
