0a3292ef9133bede62884b1f2e7494c99dc7a4b5
Cross 1.21 <you@example.com>
2016-04-29 18:47:17 +0800

    tweak:add temp output for debug

--- spider-robot_500.service.sh
+++ spider-robot_500.service.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 . /etc/profile.d/caiex.sh
 exec java -jar /opt/spider/spider-robot.jar 500 >> /dev/null 2>&1
+#exec java -jar /opt/spider/spider-robot.jar 500 >> /opt/logs/spider-robot_500_temp.out 2>&1
