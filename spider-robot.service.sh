83726e029c377d4a4e6f0e7d8902d9be363eb533
Cross 1.21 <you@example.com>
2016-06-08 19:05:35 +0800

    temp change: remove pinnacle; add javaagent to robot_jbb

--- spider-robot.service.sh
+++ spider-robot.service.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 . /etc/profile.d/caiex.sh
-exec java -jar /opt/spider/spider-robot.jar win310 pinnacle sportteryAll sporttery >> /dev/null 2>&1
+#exec java -javaagent:/opt/spider/OneAPM/oneapm.jar -jar /opt/spider/spider-robot.jar win310 pinnacle sportteryAll sporttery >> /dev/null 2>&1
 #exec java -jar /opt/spider/spider-robot.jar jbb lj win310 pinnacle sportteryAll sporttery >> /opt/logs/spider-robot_all_temp.out 2>&1
+#exec java -jar /opt/spider/spider-robot.jar win310 pinnacle sportteryAll sporttery >> /dev/null 2>&1
+exec java -jar /opt/spider/spider-robot.jar win310 sportteryAll sporttery >> /dev/null 2>&1 #remove pinnacle temporarly
--- spider-robot_500.service.sh
+++ spider-robot_500.service.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-. /etc/profile.d/caiex.sh
-exec java -jar /opt/spider/spider-robot.jar 500 >> /dev/null 2>&1
-#exec java -jar /opt/spider/spider-robot.jar 500 >> /opt/logs/spider-robot_500_temp.out 2>&1
+. /etc/profile
+#exec java -javaagent:/opt/spider/OneAPM/oneapm.jar -jar /opt/spider/spider-robot.jar 500 >> /dev/null 2>&1
+
+#exec java -javaagent:/opt/spider/OneAPM/oneapm.jar -jar /opt/spider/spider-robot.jar 500 >> /opt/logs/spider-robot_500_temp.out 2>&1
+exec java -jar /opt/spider/spider-robot.jar 500 >> /opt/logs/spider-robot_500_temp.out >> /dev/null 2>&1
