a29e91ff7202e7b150fcf35039c5d5b90cbe6a0b
Cross <drcross@outlook.com>
2016-06-17 19:30:58 +0800

    change: redis start script

--- redis_6479_upstart.sh
+++ redis_6479_upstart.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -ex
-. /etc/profile.d/caiex.sh
+. /etc/profile
+cd $REDIS_HOME
 #exec $REDIS_HOME/26479/redis-sentinel $REDIS_HOME/26479/sentinel.conf >> /opt/logs/redis.out 2>&1 
 exec $REDIS_HOME/6479/redis-server $REDIS_HOME/6479/redis.conf >> /opt/logs/redis.out 2>&1 
 #exec $REDIS_HOME/6579/redis-server $REDIS_HOME/6579/redis.conf >> /opt/logs/redis.out 2>&1 
