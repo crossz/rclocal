014afe58b2abd8c095d0f9fa127d580aa0a6d752
Cross_Could6 <zx@caiex.com>
2016-06-20 19:06:16 +0800

    correct: Chelsea_Nie's messy

--- redis_start_sentinel.sh
+++ redis_start_sentinel.sh
@@ -2,3 +2,7 @@
 systemctl start redis_6479
 systemctl start redis_6579
 systemctl start redis_26479.service
+<<<<<<< Updated upstream
+=======
+systemctl start redis_26579.service
+>>>>>>> Stashed changes
