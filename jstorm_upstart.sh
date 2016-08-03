a1d1750411a30d9df1d82cf7529a824db63e8f19
Cross@official <zx@caiex.com>
2016-08-03 10:44:52 +0800

    adjust jstorm.sh

--- jstorm_upstart.sh
+++ jstorm_upstart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 . /etc/profile.d/caiex.sh
-exec /opt/deploy/jstorm/bin/jstorm jar /opt/jstorm/rm-0.0.1-SNAPSHOT.jar com.caiex.storm.TestStormStart >> /opt/logs/jstorm.out 2>&1 
+exec /opt/jstorm-2.1.1/bin/jstorm jar /opt/jstorm/rm-0.0.1-SNAPSHOT.jar com.caiex.storm.TestStormStart >> /opt/logs/jstorm.out 2>&1 
 #exec jstorm jar /opt/jstorm/rm.jar com.caiex.storm.TestStormStart test >> /opt/logs/jstorm.out 2>&1 
 #exec jstorm jar /opt/jstorm/rm-0.0.1-SNAPSHOT.jar com.caiex.storm.TestStormStart test
 
--- mqbroker_upstart.sh
+++ mqbroker_upstart.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 . /etc/profile
 
-### global variables
-NAMESRV_ADDR='127.0.0.1:9876'
-export NAMESRV_ADDR
+## global variables
+NAMESRV_ADDR='10.171.127.205:9876'
 
 exec $ROCKETMQ_HOME/bin/mqbroker -n $NAMESRV_ADDR >> /opt/logs/rmq.out 2>&1 
