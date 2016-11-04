5b869c215917deea56349613440a72900c992cc4
zx root@aaa <zx@caiex.com>
2016-11-04 12:41:43 +0800

    archive: mqbroker

--- mqbroker_upstart.sh
+++ mqbroker_upstart.sh
@@ -5,4 +5,5 @@
 NAMESRV_ADDR='10.174.11.72:9876'
 export NAMESRV_ADDR
 
+echo 'name server is: '$NAMESRV_ADDR >> /opt/logs/rmq.out
 exec $ROCKETMQ_HOME/bin/mqbroker -n $NAMESRV_ADDR -c /opt/alibaba-rocketmq/conf/2m-noslave/broker-a.properties >> /opt/logs/rmq.out 2>&1 
