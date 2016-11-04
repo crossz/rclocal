fb12a9da763b82f59f94b0fdbacd9da13ba842f8
Cross_Could6 <zx@caiex.com>
2016-11-04 12:43:36 +0800

    slightly adjust: mqbroker

--- mqbroker_upstart.sh
+++ mqbroker_upstart.sh
@@ -6,5 +6,4 @@ NAMESRV_ADDR='10.174.11.72:9876'
 export NAMESRV_ADDR
 
 echo 'name server is: '$NAMESRV_ADDR >> /opt/logs/rmq.out
-exec $ROCKETMQ_HOME/bin/mqbroker -n $NAMESRV_ADDR -c /opt/alibaba-rocketmq/conf/2m-noslave/broker-a.properties >> /opt/logs/rm
-q.out 2>&1
+exec $ROCKETMQ_HOME/bin/mqbroker -n $NAMESRV_ADDR -c /opt/alibaba-rocketmq/conf/2m-noslave/broker-a.properties >> /opt/logs/rmq.out 2>&1
