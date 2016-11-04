7514a7c77fee0a32564be44fb27e61aa8870b351
crossz <chelsea_nie>
2016-11-04 16:03:16 +0800

    correct: game-*.sh with &, this causes systemd failure

--- game-service.sh
+++ game-service.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 . /etc/profile
-exec java -jar /opt/games/GAME-SERVICE-1.0.jar >> /opt/logs/games/game-serv.log 2>&1 & 
+exec java -jar /opt/games/GAME-SERVICE-1.0.jar >> /opt/logs/game-service.out 2>&1
--- tomcat_upstart.sh
+++ tomcat_upstart.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 . /etc/profile
-exec $CATALINA_HOME/bin/catalina.sh run >> /opt/logs/catalina-api.out 2>&1
+exec $CATALINA_HOME/bin/catalina.sh run >> /opt/logs/catalina.out 2>&1
