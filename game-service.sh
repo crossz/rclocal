0b63aa4ecb0f07b826bbfb82ed89eed3dffef0de
Cross_Could6 <zx@caiex.com>
2016-11-04 12:35:09 +0800

    adjust: game-service and game-crawler

--- game-service.sh
+++ game-service.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 . /etc/profile
-#exec nohup java -javaagent:/opt/apache-tomcat-7.0.63/OneAPM/oneapm.jar -jar GAME-SERVICE-1.0.jar >> /opt/games/logs/gameser.log 2>&1 & 
-exec java -jar /opt/games/GAME-SERVICE-1.0.jar >> /opt/games/logs/gameser.log 2>&1 & 
+exec java -jar /opt/games/GAME-SERVICE-1.0.jar >> /opt/logs/games/game-serv.log 2>&1 & 
--- sbcconsumer_upstart.sh
+++ sbcconsumer_upstart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-. /etc/profile.d/caiex.sh
-. /opt/sbcconsumer/profile.d/caiex_matlab.sh
-#exec java -jar /opt/sbcconsumer/SBC-0.0.1-SNAPSHOT.jar >> /opt/logs/sbcconsumer.out 2>&1 
-exec java -jar /opt/sbcconsumer/SBC-MAT-1.0.jar >> /opt/logs/sbcconsumer.out 2>&1
+. /etc/profile
+. /opt/SBCCONSUMER/profile.d/caiex_matlab.sh
+
+exec java -jar /opt/SBCCONSUMER/SBC-MAT-1.0.jar >> /opt/logs/sbcconsumer.out 2>&1
