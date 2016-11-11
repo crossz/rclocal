97eec6560060b85d223f35556e64a7485e625870
zx root@aaa <zx@caiex.com>
2016-11-11 11:07:57 +0800

    add:spider-robot restart ...

---
+++ spider-robot-restartljjbb.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -ex
+service spider-robot_jbb restart
+echo 'spider robot jbb restart'
+service spider-robot_lj restart
+echo 'spider robot lj restart'
+
+## manual robot+500
+service spider-robot_manual restart
+echo 'spider robot manual restart'
+systemctl restart spider-500w-manual
+
+echo "--------==== All restarted ====--------"
