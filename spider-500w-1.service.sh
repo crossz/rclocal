eeb4376293540a3a928f764bf326c69dbf25323d
zx root@aaa <zx@caiex.com>
2016-11-13 23:29:12 +0800

    add: proxy for phantom.js 500.js

--- spider-500w-1.service.sh
+++ spider-500w-1.service.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export HTTP_PROXY=188.166.174.133:3128
 cd /opt/spider/500
-exec phantomjs /opt/spider/500/500.js -1 utf-8 >> /opt/logs/w500.log 2>&1
+exec phantomjs /opt/spider/500/500.js -1 utf-8 >> /opt/logs/w500-1.log 2>&1
 
--- spider-500w.service.sh
+++ spider-500w.service.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export HTTP_PROXY=188.166.174.133:3128
 cd /opt/spider/500
 exec phantomjs /opt/spider/500/500.js 0 utf-8 >> /opt/logs/w500.log 2>&1
 
