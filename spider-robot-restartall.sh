c87e588406339a067fc5de2913f80dfd34ebc894
Cross 1.21 <you@example.com>
2016-11-19 16:37:32 +0800

    clean: spier-robot_*

--- spider-robot-restartall.sh
+++ spider-robot-restartall.sh
@@ -3,16 +3,25 @@ service spider-robot restart
 echo 'spider robot restart'
 service spider-robot_stats restart
 echo 'spider robot stats restart'
-service spider-robot_500 restart
-echo 'spider robot 500 restart'
 service spider-robot_jbb restart
 echo 'spider robot jbb restart'
 service spider-robot_lj restart
 echo 'spider robot lj restart'
 
+
+service spider-robot_500 restart
+echo 'spider robot 500 restart'
+systemctl restart spider-500w
+systemctl restart spider-500w-1
+
+
+## manual robot
 service spider-robot_manual restart
 echo 'spider robot manual restart'
+systemctl restart spider-500w-manual
 
-####
+### clean robot
 #service spider-robot_clean restart
 #echo 'spider robot clean restart'
+
+echo "--------==== All restarted ====--------"
