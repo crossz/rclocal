64286a4f19f41d4f58d3c2db233e54ffd111c854
Cross 1.21 <you@example.com>
2017-01-04 13:50:29 +0800

    change: path of 500.js

--- spider-500w-1.service.sh
+++ spider-500w-1.service.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #export http_proxy=http://188.166.174.133:33128
-cd /opt/spider/500
-exec phantomjs /opt/spider/500/500.js -1 utf-8 >> /opt/logs/w500-1.log 2>&1
+cd /opt/spider/
+exec phantomjs /opt/spider/500.js -1 utf-8 >> /opt/logs/w500-1.log 2>&1
 
