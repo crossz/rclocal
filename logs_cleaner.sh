ad45c62f633a8ffaa71c364b4bc8b6d27710bad6
zx root@aaa <zx@caiex.com>
2017-01-10 18:01:57 +0800

    move: robot_500 and phantom for 500,500-1 to docker; adjust: log_cleaner.sh.

--- logs_cleaner.sh
+++ logs_cleaner.sh
@@ -3,6 +3,12 @@
 cd /opt/logs
 rm -rf sbcconsumer.out.201*
 rm -rf catalina-api.out.201*
+rm -rf catalina-sbc.out.201*
+rm -rf dubbo.out.201*
+rm -rf consumer.out.201*
+
+rm -rf centralserver.out.201*
+rm -rf spider-web.out.201*
 
 cd /opt/logs/spider-robot
 rm -rf *.log.201*
