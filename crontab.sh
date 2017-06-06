74335e2b7130bab6d45abeb7057f3596de0e3f10
zx root@aaa <zx@caiex.com>
2017-06-06 14:04:21 +0800

    adjust: crontab.sh

--- crontab.sh
+++ crontab.sh
@@ -4,5 +4,7 @@ cat /dev/null > /opt/logs/catalina-api.out
 cat /dev/null > /opt/logs/dubbo.out
 cat /dev/null > /opt/logs/consumer.out
 cat /dev/null > /opt/logs/sbcconsumer.out
+cat /dev/null > /opt/logs/sbcconsumer2/sbcconsumer.out
 cat /dev/null > /opt/logs/centralserver.out
 cat /dev/null > /opt/logs/spider-web.out
+cat /dev/null > /opt/logs/linechart.out
