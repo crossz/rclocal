56eaf4dcf7b570ac7e7f1a37efcf01ba00f02ea4
Cross 1.21 <you@example.com>
2017-08-02 17:13:23 +0800

    add: all systemd scripts for spider on 1.21.

--- spider-robot.service.sh
+++ spider-robot.service.sh
@@ -3,4 +3,4 @@
 #exec java -javaagent:/opt/spider/OneAPM/oneapm.jar -jar /opt/spider/spider-robot.jar win310 pinnacle sportteryAll sporttery >> /dev/null 2>&1
 #exec java -jar /opt/spider/spider-robot.jar jbb lj win310 pinnacle sportteryAll sporttery >> /opt/logs/spider-robot_all_temp.out 2>&1
 #exec java -jar /opt/spider/spider-robot.jar win310 pinnacle sportteryAll sporttery >> /dev/null 2>&1
-exec java -jar /opt/spider/spider-robot.jar win310 sportteryAll sporttery >> /dev/null 2>&1 #remove pinnacle temporarly
+exec java -jar /opt/spider/spider-robot.jar win310 sportteryAll sporttery statistic >> /dev/null 2>&1 #remove pinnacle temporarly
